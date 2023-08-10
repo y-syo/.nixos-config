2f07064bd90f08347d81f88399dc60d0ea18f8ce
y-syo <mmoussou@student.42angouleme.fr>
2023-08-10 23:03:04 +0200

    feat(weebsyo): added honkers

--- flake.nix
+++ flake.nix
@@ -19,16 +19,30 @@
 
     # gross
     gross.url = "github:fufexan/gross";
+
+    # HSR & GI
+    aagl.url = "github:ezKEa/aagl-gtk-on-nix";
+    aagl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, hyprsome, stylix, home-manager, gross, ... }: {
+  nixConfig = {
+    extra-substituters = [ "https://ezkea.cachix.org" ];
+    extra-trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+  };
+
+  outputs = inputs@{ self, nixpkgs, hyprsome, stylix, home-manager, gross, aagl, ... }: {
     nixosConfigurations = {
       outerwilds = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
         specialArgs = { inherit inputs; };
         modules = [
-          ./configuration.nix
+	  {
+		imports = [ aagl.nixosModules.default ];
+		nix.settings = aagl.nixConfig; # Set up Cachix
+		programs.honkers-railway-launcher.enable = true;
+	  }
+	  ./configuration.nix
         ];
       };
     };
