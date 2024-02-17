67067626d99dbfae6668bfbfc9d13ebbf3c8d1eb
y-syo <mmoussou@student.42angouleme.fr>
2024-02-17 18:09:14 +0100

    [🔨] fix(pkgs/pogit/default.nix): fixed a typo

--- pkgs/pogit/default.nix
+++ pkgs/pogit/default.nix
@@ -1,10 +1,10 @@
-{ stdenv, lib, writeText, fetchFromGithub, python3Packages }:
+{ stdenv, lib, writeText, fetchFromGitHub, python3Packages }:
 
 python3Packages.buildPythonApplication rec {
   pname = "pogit";
   version = "git";
 
-  src = fetchFromGithub {
+  src = fetchFromGitHub {
 	owner = "y-syo";
 	repo = "pogit";
 	rev = "0b04dac5ea92c3bd9b52bd21800d3fdd51e92b1b";
