436f84f91379430ce89f239c5a4c57d0c70f55f5
y-syo <mmoussou@student.42angouleme.fr>
2024-12-17 20:58:37 +0100

    「✨」 feat(nurture/hw): added 32GB of swapfile

--- hosts/nurture/hardware-configuration.nix
+++ hosts/nurture/hardware-configuration.nix
@@ -24,7 +24,13 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+      device = "/swapfile";
+      size = 32 * 1024; # 32GB
+    }
+  ];
+
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
