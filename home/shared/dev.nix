8edb4d08fa3f962199eeb081330375ae464e6609
y-syo <mmoussou@student.42angouleme.fr>
2025-02-04 05:09:54 +0100

    「🔨」 fix(pogit.nix): fixed pogit config

--- home/shared/dev.nix
+++ home/shared/dev.nix
@@ -17,15 +17,15 @@
       config = {
         nix = {
           icon = "❄️";
-          default_msg = "nixed the project.";
+          msg = "nixed the project.";
         };
         feet = {
           icon = "🦶";
-          default_msg = "added feets to the project, now it walks.";
+          msg = "added feets to the project, now it walks.";
         };
         wut = {
           icon = "❓";
-          default_msg = "i have no clue what's going on anymore...";
+          msg = "i have no clue what's going on anymore...";
         };
       };
     };
